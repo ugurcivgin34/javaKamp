b91b4a345b79f64bda63743136adbef799a45983
okan çivgin <okanbjkspd@hotmail.com>
2021-05-28 00:21:23 +0300

    8.Ders Sonu

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -11,20 +11,20 @@ import kodlamaio.northwind.entitites.concretes.Product;
 @Repository
 public interface ProductDao extends JpaRepository<Product, Integer> { // Entity de product ile ilişkilendir, id integer
 																		// olduğu için ıngeter olarak belirttik
-	Product getByProductName(String productName);
-
-	Product getByProductNameAndCategory(String productName, int categoryId);
-	
-	List<Product> getByProductNameOrCategory(String productName,int categoryId);
-	
-	List<Product> getByCategoryIn(List<Integer>categories);
-	
-	List<Product> getByProductNameContains(String productName);
-	
-	List<Product> getByProductNameStartsWith(String productName);
-	
-	@Query("From Product where productName=:productName and category.categoryId=:categoryId")
-	List<Product> getByNameAndCategory(String productName,int categoryId);
-	
-	//select * from products where product_name=bisey and categoryId=bisey
+	 Product getByProductName(String productName);
+	  
+	  Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
+	  
+	  List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
+	  
+	  List<Product> getByCategoryIn(List<Integer> categories);
+	  
+	  List<Product> getByProductNameContains(String productName);
+	  
+	  List<Product> getByProductNameStartsWith(String productName);
+	  
+	  @Query("From Product where productName=:productName and category.categoryId=:categoryId")
+	  List<Product> getByNameAndCategory(String productName, int categoryId);
+	  
+	  //select * from products where product_name=bisey and categoryId=bisey
 }
