0bc499607000676b22ccd65ed18266f268ac8af1
okan çivgin <okanbjkspd@hotmail.com>
2021-05-27 22:26:48 +0300

    8.Ders

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -1,9 +1,30 @@
 package kodlamaio.northwind.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import kodlamaio.northwind.entitites.concretes.Product;
 
-public interface ProductDao extends JpaRepository<Product, Integer> { //Entity de product ile ilişkilendir, id integer olduğu için ıngeter olarak belirttik
+@Repository
+public interface ProductDao extends JpaRepository<Product, Integer> { // Entity de product ile ilişkilendir, id integer
+																		// olduğu için ıngeter olarak belirttik
+	Product getByProductName(String productName);
 
+	Product getByProductNameAndCategoryId(String productName, int categoryId);
+	
+	List<Product> getByProductNameOrCategoryId(String productName,int categoryId);
+	
+	List<Product> getByCategoryIdIn(List<Integer>categories);
+	
+	List<Product> getByProductNameContains(String productName);
+	
+	List<Product> getByProductNameStartsWith(String productName);
+	
+	@Query("From Product where productName=:productName and categoryId=:categoryId")
+	List<Product> getByNameAndCategory(String productName,int categoryId);
+	
+	//select * from products where product_name=bisey and categoryId=bisey
 }
